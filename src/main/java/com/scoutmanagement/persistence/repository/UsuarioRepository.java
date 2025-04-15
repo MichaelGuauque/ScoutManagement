7793f71c17f3acc9dc298497c0738426f3e52a8a
MichaelGuauque <mcalejo98@gmail.com>
2025-04-14 20:12:05 -0500

    Creacion model y repository de usuario

---
+++ src/main/java/com/scoutmanagement/persistence/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.scoutmanagement.persistence.repository;
+
+import com.scoutmanagement.persistence.model.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    UserDetails findByUsername(String username);
+}
