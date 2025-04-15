03edcaebe9943adc786d3a02d06bfcd0e77970b9
MichaelGuauque <mcalejo98@gmail.com>
2025-04-14 19:33:58 -0500

    exclude fly

---
+++ src/main/java/com/scoutmanagement/persistence/model/RoleEnum.java
@@ -0,0 +1,5 @@
+package com.scoutmanagement.persistence.model;
+
+public enum RoleEnum {
+    ADMIN, USER
+}
---
+++ src/main/java/com/scoutmanagement/persistence/model/RolePermiso.java
@@ -0,0 +1,27 @@
+package com.scoutmanagement.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class RolePermiso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleEnum;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_permission", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permiso> permissions = new HashSet<>();
+}
