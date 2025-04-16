b5e6b44301c0b8cc47d787efa6de3f66d22211a0
Atse <MiguelAngelo_lll@hotmail.com>
2025-04-16 16:29:17 -0500

    feact: DTO y Service

---
+++ src/main/java/com/scoutmanagement/service/implementation/PersonaService.java
@@ -0,0 +1,42 @@
+package com.scoutmanagement.service.implementation;
+
+import com.scoutmanagement.DTO.PersonaDTO;
+import com.scoutmanagement.persistence.model.*;
+import com.scoutmanagement.persistence.repository.PersonaRepository;
+import com.scoutmanagement.service.interfaces.IPersonaService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService implements IPersonaService {
+
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    @Override
+    public void save(PersonaDTO personaDTO) {
+        personaRepository.save(CambiarRegistroPersonaDTO(personaDTO));
+    }
+
+    public  Persona CambiarRegistroPersonaDTO(PersonaDTO personaDTO){
+        Persona persona= Persona.builder()
+                .primerNombre(personaDTO.primerNombre())
+                .segundoNombre(personaDTO.segundoNombre())
+                .primerApellido(personaDTO.primerApellido())
+                .segundoApellido(personaDTO.segundoApellido())
+                .numeroDeDocumento(personaDTO.numeroDeDocumento())
+                .tipoDeDocumento(personaDTO.tipoDeDocumento())
+                .genero(personaDTO.genero())
+                .rama(personaDTO.rama())
+                .cargo(personaDTO.cargo())
+                .rol(personaDTO.rol())
+                .build();
+
+        return persona;
+
+        }
+}
+
---
+++ src/main/java/com/scoutmanagement/service/interfaces/IPersonaService.java
@@ -0,0 +1,7 @@
+package com.scoutmanagement.service.interfaces;
+
+import com.scoutmanagement.DTO.PersonaDTO;
+
+public interface IPersonaService {
+    void save(PersonaDTO personaDTO);
+}
