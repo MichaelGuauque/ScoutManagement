f8d5a260867c8218d0c74aa082c1fd29369ca464
Paul Figueroa <paul.figueroa@correounivalle.edu.co>
2025-04-17 20:32:08 -0500

    refactor front HU14

--- src/main/java/com/scoutmanagement/controller/ActividadController.java
+++ src/main/java/com/scoutmanagement/controller/ActividadController.java
@@ -1,20 +1,23 @@
 package com.scoutmanagement.controller;
 
 import com.scoutmanagement.DTO.ActividadDTO;
-import com.scoutmanagement.persistence.model.*;
+import com.scoutmanagement.persistence.model.Actividad;
+import com.scoutmanagement.persistence.model.Rama;
 import com.scoutmanagement.service.interfaces.IActividadService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -27,11 +30,23 @@ public class ActividadController {
     private IActividadService actividadService;
 
     @GetMapping()
-    public String actividades(Model model) {
+    public String actividades(Model model,@RequestParam(required = false, defaultValue = "proximas") String tab) {
         List<Actividad> listaActividades = actividadService.findAllActividad();
-        model.addAttribute("actividades", listaActividades);
-        model.addAttribute("ramas", Rama.values());
+        LocalDate hoy = LocalDate.now();
+        List<Actividad> actividadesFiltradas;
 
+        if (tab.equals("pasadas")) {
+            actividadesFiltradas = listaActividades.stream()
+                    .filter(actividad -> actividad.getFecha().isBefore(hoy))
+                    .collect(Collectors.toList());
+        } else {
+            actividadesFiltradas = listaActividades.stream()
+                    .filter(actividad -> !actividad.getFecha().isBefore(hoy)) // hoy o después
+                    .collect(Collectors.toList());
+        }
+
+        model.addAttribute("tabSeleccionada", tab);
+        model.addAttribute("actividades", actividadesFiltradas);
         return "actividades/vistaActividadesAdmin";
     }
 
@@ -48,8 +63,4 @@ public class ActividadController {
         actividadService.crearActividad(actividadDTO);
         return "redirect:/actividades";
     }
-
-
-
-
 }
