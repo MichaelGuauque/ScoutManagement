64c00e45ce301a0ebc221ad7acd20eb47dc7314a
Miguel Andres MF <miguelandresmarinf@gmail.com>
2025-04-20 13:10:26 -0500

    Refactor: Actividad(Modelo, Front)

--- src/main/java/com/scoutmanagement/persistence/model/Actividad.java
+++ src/main/java/com/scoutmanagement/persistence/model/Actividad.java
@@ -17,10 +17,10 @@ public class Actividad {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
     private String nombre;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "VARCHAR(500)")
     private String descripcion;
 
     @Enumerated(EnumType.STRING)
@@ -29,7 +29,7 @@ public class Actividad {
     @Column(columnDefinition = "DATE")
     private LocalDate fecha;
 
-    @Column(nullable = false)
+    @Column(nullable = true, columnDefinition = "VARCHAR(200)")
     private String ubicacion;
 
 }
