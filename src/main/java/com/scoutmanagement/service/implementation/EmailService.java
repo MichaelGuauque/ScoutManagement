a999c5c2682bacd699b7df9db0903bfda5c0c965
Atse <MiguelAngelo_lll@hotmail.com>
2025-07-04 10:35:43 -0500

    Refactor: Correo que se recibe al crear Miembro y el tipo de Documento ahora no esta abreviado en el formulario

--- src/main/java/com/scoutmanagement/service/implementation/EmailService.java
+++ src/main/java/com/scoutmanagement/service/implementation/EmailService.java
@@ -4,7 +4,6 @@ import com.scoutmanagement.util.exception.ServiceException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
